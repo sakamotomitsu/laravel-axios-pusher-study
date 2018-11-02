d5024ca90bab9c885e4520eb7b430b1516022449
sakamotomitsu <sakamotomitsuki000@gmail.com>
2018-11-02 20:51:29 +0900

    Ajax

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -3,7 +3,7 @@
 <div id="chat">
   <textarea v-model="message"></textarea>
   <br>
-  <button type="button">送信</button>
+  <button type="button" @click="send()">送信</button>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.18.0/axios.min.js"></script>
@@ -13,6 +13,21 @@
     el: '#chat',
     data: {
       message: ''
+    },
+    methods: {
+      send() {
+
+        const url = '/ajax/chat';
+        const params = { message: this.message };
+        axios.post(url, params)
+        .then((response) => {
+
+          // 成功したらメッセージをクリア
+          this.message = '';
+
+        });
+
+      }
     }
   });
 
